b2baa92a43c2e343b34d7a0a7ab908532b335467
Gonzalo Portilla <gonzalo.portilla@opendeusto.es>
2024-09-23 08:21:39 +0200

    tercer mensaje

--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Se termina la clase");
 		System.out.println("Se termina la clase2");
-
+		System.out.println("tercero");
 	}
 
 }
